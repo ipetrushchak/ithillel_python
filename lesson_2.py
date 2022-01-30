bc33bf5a86c10b4ee1862d2bb4f382d507ac6c8a
ipetrushchak <ipetruschak@gmail.com>
2022-01-30 18:11:50 +0200

    renamed file

---
+++ lesson_2.py
@@ -0,0 +1,36 @@
+'''
+
+'''
+# your_name = input("Please type your name: ")
+# print(f"Hi,{your_name}")
+#
+# str_list = "Welcome to Lesson#2"  #str(year)
+# year = 2022
+# pi = 3.14
+# c_num = 3j+10
+# my_list = ["A", "B", "C", "D"]
+# my_tuple = ("A", "B", "C", "D")
+# my_dict = {"A":"a", "B":"b", "C":"c", "D":"d"}
+# my_set = {'A', 'B', 'C', 'D'}
+#
+# print("The type is : ",type(str_list))
+# print("The type is : ",type(year))
+# print("The type is : ",type(pi))
+# print("The type is : ",type(c_num))
+# print("The type is : ",type(my_list))
+# print("The type is : ",type(my_tuple))
+# print("The type is : ",type(my_dict))
+# print("The type is : ",type(my_set))
+# str_list = "Welcome to Lesson#2"
+# igor = 'Igor'
+# new_var = (1, 6, 6, 6, 3, 45, 4, 4, igor, 5,"Igor", "Ivan")
+#
+# print(str(new_var))
+# print("Helle\bo World")
+import constants
+print(constants.PI)
+print(constants.GRAVITY)
+
+"""
+
+"""
