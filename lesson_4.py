afb580566b9be652a1585454f8ea55113410b8c6
ipetrushchak <ipetruschak@gmail.com>
2022-02-05 17:24:03 +0200

    Lesson#4 updated

--- lesson_4.py
+++ lesson_4.py
@@ -1,12 +1,24 @@
+mylist = ['apple', 'banana', 'cherry']
+print(len(mylist))
+
+string = 'Hello'
+len(string)
+
+
+d = {'a': 1, 'b': 1, 'c': 1, 'd': 1}
+keys = list(d)
+len(keys)
+#
+#
 # for number in [0, 1, 2, 3, 4]:
 #     print(number)
 #
 # # ==================================
 #
-for num in range(5):
-    print(num)
-else:
-    print("Числа закончились")
+# for num in range(5):
+#     print(num)
+# else:
+#     print("Числа закончились")
 
 # ==================================
 # for num in range(5):
@@ -17,32 +29,32 @@ else:
 # else:
 #     print("Числа закончились")
 # # ==================================
-
-for num in range(5):
-    if num == 3:
-        continue
-    else:
-        print(num)
-else:
-    print("Числа закончились")
+#
+# for num in range(5):
+#     if num == 3:
+#         continue
+#     else:
+#         print(num)
+# else:
+#     print("Числа закончились")
 
 # ====================================
-i = 0
-while i < 5:
-    print(i)
-    i += 1
-else:
-    print("Конец")
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
+# else:
+#     print("Конец")
 # # ====================================
-i = 0
-while i < 5:
-    if i == 3:
-        break
-    else:
-        print(i)
-        i += 1
-else:
-    print("Конец")
+# i = 0
+# while i < 5:
+#     if i == 3:
+#         break
+#     else:
+#         print(i)
+#         i += 1
+# else:
+#     print("Конец")
 # # ====================================
 # i = 0
 # while i < 5:
