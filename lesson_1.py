b6ab247d1c228dd48fc1f3ce152b0f92a8602172
KYIV\Igor_Petrushchak <igor_petrushchak@epam.com>
2022-02-23 21:08:18 +0200

    lesson_9.py updated

--- lesson_1.py
+++ lesson_1.py
@@ -1,8 +1,1 @@
-input_names = "Mira Mira Mira Maya Lena  Maya Lusya Maksimus Maya"
-famyly_namber = {}
-for famyly in input_names.split():
-    try:
-        famyly_namber[famyly] += 1
-    except KeyError:
-        famyly_namber[famyly] = 1
-print(famyly_namber)
+# =
